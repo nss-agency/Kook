f65d0797d1f94f6151c7d267d539fc23e36c2984
Korb1t <oles.hobor@gmail.com>
2020-03-05 17:47:03 +0200

    Add MenuItem model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from phone_field import PhoneField
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 
 
 # Create your models here.
@@ -27,3 +29,25 @@ class Booking(models.Model):
     class Meta:
         verbose_name = 'Замовлення'
         verbose_name_plural = 'Замовлення'
+
+
+class MenuItem(models.Model):
+    image = models.ImageField('Зображення', upload_to='menu_images')
+    title = models.CharField('Назва страви', max_length=225)
+    description = models.TextField('Опис')
+    price = models.IntegerField('Ціна')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукти'
+
+
+@receiver(post_delete)
+def submission_delete(sender, instance, **kwargs):
+    try:
+        instance.image.delete(False)
+    except AttributeError:
+        pass
