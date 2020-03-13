8e421cbec007a93ce2822b0c4c9e5b0c710f5f62
DigitalG <Thesever99@gmail.com>
2020-03-13 12:52:47 +0200

    merge

--- core/views.py
+++ core/views.py
@@ -5,16 +5,6 @@ from .decorators import check_recaptcha
 import datetime
 
 
-# def is_room_type_available(room_type, date_entry, date_leave):
-#     # case 1: a room is booked before the check_in date, and checks out after the requested check_in date
-#
-
-def hotel(request):
-    ctx = {}
-
-    return render(request, 'hotel_rooms.html', ctx)
-
-
 # Create your views here.
 def index(request):
     ctx = {}
@@ -23,16 +13,9 @@ def index(request):
 
 
 def hotel(request):
-    ctx = {}
-
-    return render(request, 'hotel_rooms.html', ctx)
-
-
-def form(request):
     ctx2 = {
         'success': False,
         'fail': False,
-
     }
     if request.method == 'POST':
         form = BookingForm(request.POST)
@@ -40,7 +23,53 @@ def form(request):
             ctx2 = {
                 'success': False,
                 'fail': False,
+            }
+            date_entry = form.cleaned_data['date_entry']
+            date_leave = form.cleaned_data['date_leave']
+            room_type = form.cleaned_data['room_type']
 
+            case_1 = Booking.objects.filter(room_type=room_type, date_entry__lte=date_entry,
+                                            date_leave__gte=date_entry)
+
+            case_2 = Booking.objects.filter(room_type=room_type, date_entry__lte=date_leave,
+                                            date_leave__gte=date_leave)
+
+            case_3 = Booking.objects.filter(room_type=room_type, date_entry__lte=date_entry,
+                                            date_leave__gte=date_leave)
+
+            case_4 = Booking.objects.filter(room_type=room_type, date_entry__gte=date_entry,
+                                            date_leave__lte=date_leave)
+
+            case = (case_1.union(case_2).union(case_3).union(case_4)).count()
+
+            if (case_1 or case_2 or case_3 or case_4) and case >= room_type.quantity:
+                print('Zanyato')
+                ctx2['fail'] = True
+            else:
+                booking = form.save(commit=False)
+                booking.save()
+                ctx2['success'] = True
+        else:
+            BookingForm()
+
+    ctx = {
+        'form': BookingForm,
+        'ctx2': ctx2
+    }
+    return render(request, 'hotel_rooms.html', ctx)
+
+
+def form(request):
+    ctx2 = {
+        'success': False,
+        'fail': False,
+    }
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            ctx2 = {
+                'success': False,
+                'fail': False,
             }
             date_entry = form.cleaned_data['date_entry']
             date_leave = form.cleaned_data['date_leave']
